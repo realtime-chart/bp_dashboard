14a4d6caa0ff12871e5998dfeef0e79edbbe2f26
Paul Asmuth <paul@23linesofcode.com>
2011-06-03 22:53:25 +0200

    stacks of blocks

--- src/scene_2.js
+++ src/scene_2.js
@@ -3,8 +3,13 @@ scene_2 = {
 	cube_width: 1,
 	cube_height: 1,
 	cube_spacing: 2.5,
-	cubes: new Array,	
-
+	cubes: [ [] ],
+	cubes_num: 0,
+	cube_lvl: 0,
+	cube_widths: [ 1 ],
+	
+	camera_pos: 0,
+	
 	initScene: function(){
 		gl.clearColor((99/255.0), (107/255.0), (112/255.0), 1.0);
 		gl.clearDepth(1.0);
@@ -16,23 +21,65 @@ scene_2 = {
 
 		/*createObject('box_2', 'box');
 		moveObjectAbs('box_2', [1.5, 0, 0]);	*/		
+		
+		this.addCube();
+		this.addCube();
+		this.addCube();
+		this.addCube();
+		this.addCube();
+		this.addCube();
+		this.addCube();
+		this.addCube();
+		this.addCube();
+		this.cubes.push(new Array)
+		this.cube_lvl += 1;
+		this.cube_widths.push(1);
+		this.addCube();
+		this.addCube();
+		this.addCube();
+		this.addCube();
+		this.addCube();
+		this.addCube();
+		this.addCube();
+		this.addCube();
+		this.addCube();
 	},
 
-	calculateScene: function(diff){
-		this.addCube();
-		world.camera.position = [0,0,-100];
-		world.camera.rotation = [30,30,0];
+	calculateScene: function(diff){		
+		world.camera.position = [0,0,-80];
+		world.camera.rotation = [30,this.camera_pos,0];
+		this.camera_pos += 0.002 * diff;
 	},
 	
 	addCube: function(){	
-		pos = this.nextCubePos();				
-		var t = 'box_'+this.cubes.length;
+		var pos = this.nextCubePos();				
+		var t = 'box_'+this.cubes_num;
 		createObject(t, 'box');
-		moveObjectAbs(t, [(pos[0]*this.cube_spacing), 0, (pos[2]*this.cube_spacing)]);
-		this.cubes.push(t);
+		moveObjectAbs(t, [(pos[0]*this.cube_spacing), (pos[1]*this.cube_spacing), (pos[2]*this.cube_spacing)]);		
+		this.cubes[this.cube_lvl].push(t);
+		this.cubes_num += 1;
 	},
 	
 	nextCubePos: function(){
+		if(this.cubes[this.cube_lvl].length>=(this.cube_widths[this.cube_lvl]*this.cube_widths[this.cube_lvl])){
+			this.cube_widths[this.cube_lvl] += 1;
+		}		
+		return this.nextCubeLvlPos(this.cube_lvl, this.cube_widths[this.cube_lvl], this.cubes[this.cube_lvl]);
+	},
+	
+	nextCubeLvlPos: function(lvl, width, lvl_cubes){
+		var n = lvl_cubes.length;
+		var total = (width*width);
+		var total_xz = (width*width);
+		var row_c = n - ((width-1)*(width-1));
+		if(row_c>width-1){			
+			return [width-1-((row_c-width)+1), lvl, width-1];
+		} else {
+			return [width-1, lvl, width-(width-row_c)];
+		}			
+	},
+	
+	nextCubePosOld: function(){
 		var n = this.cubes.length;
 		var total = (this.cube_width*this.cube_width);
 		if(n>=total){
