8a79ccfcc58dd8b270b411aab69deb6b58be893c
paulasmuth <paul@23loc.com>
2011-06-03 16:34:39 +0200

    sane scene

--- src/scene.js
+++ src/scene.js
@@ -6,18 +6,20 @@ function initScene(){
 }
 
 function calculateScene(diff){
-  yRot += 5 * diff;
+  //yRot += 1 * diff;
 }
 
 function drawScene(){
   var width = canvas.width = canvas.clientWidth;
-  var height = canvas.height = canvas.clientHeight;
+  var height = canvas.height = canvas.clientHeight;  
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);  
+  gl.viewport(0, 0, width, height);
   
   /*camera*/
-  pMatrix = createPerspectiveMatrix(45, width/height, fZnear, fZfar);
+  //pMatrix = createPerspectiveMatrix(45, width/height, fZnear, fZfar);
+  pMatrix = makePerspective(45, width/height, 0.1, 1000.0);
   mvMatrix = Matrix.I(4);  
-  mvMatrix = mvMatrix.x(create3DTranslationMatrix(Vector.create([0.0, 0.0, -3.0])).ensure4x4());  
+  mvMatrix = mvMatrix.x(create3DTranslationMatrix(Vector.create([0.0, -3.0, -90.0])).ensure4x4());  
   mvMatrix = mvMatrix.x(Matrix.Rotation(xRot* Math.PI / 180.0, Vector.create([1,0,0])).ensure4x4());
   mvMatrix = mvMatrix.x(Matrix.Rotation(yRot* Math.PI / 180.0, Vector.create([0,1,0])).ensure4x4());
   mvMatrix = mvMatrix.x(Matrix.Rotation(zRot* Math.PI / 180.0, Vector.create([0,0,1])).ensure4x4());
@@ -31,14 +33,16 @@ function drawScene(){
   gl.uniform3f(gl.getUniformLocation(shaderProgram, "uDirectionalColor"), 0.8, 0.8, 0.8);
  
  
+  mvTranslate([0, 0, -100]);
+ 
   /* action */
   jQuery.each(world, function(i){
-    //var obj = world['box'];    
     gl.bindBuffer(gl.ARRAY_BUFFER,this.normals);
     gl.vertexAttribPointer(vertexNormalAttribute,this.normals.itemSize, gl.FLOAT, false, 0,0);  
     gl.bindBuffer(gl.ARRAY_BUFFER,this.vertices);
     gl.vertexAttribPointer(vertexPositionAttribute,this.vertices.itemSize, gl.FLOAT, false, 0,0);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indices);  
+    
     setMatrixUniforms();  
     gl.drawElements(gl.TRIANGLES, this.indices.numItems, gl.UNSIGNED_SHORT, 0);
   });
